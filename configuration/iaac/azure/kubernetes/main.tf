665d425e4f3cf64517d0162b586d6939c89583b0
parsh <parshchakurkar@gmail.com>
2021-12-11 22:57:34 -0700

    updated main.tf

--- configuration/iaac/azure/kubernetes/main.tf
+++ configuration/iaac/azure/kubernetes/main.tf
@@ -19,6 +19,15 @@ resource "azurerm_resource_group" "resource_group" {
   location = var.location
 }
 
+
+resource "azurerm_log_analytics_workspace" "test" {
+    # The WorkSpace name has to be unique across the whole of azure, not just the current subscription/tenant.
+    name                = "${var.log_analytics_workspace_name}-${random_id.log_analytics_workspace_name_suffix.dec}"
+    location            = var.log_analytics_workspace_location
+    resource_group_name = azurerm_resource_group.k8s.name
+    sku                 = var.log_analytics_workspace_sku
+}
+
 resource "azurerm_kubernetes_cluster" "terraform-k8s" {
   name                = "${var.cluster_name}_${var.environment}"
   location            = azurerm_resource_group.resource_group.location
